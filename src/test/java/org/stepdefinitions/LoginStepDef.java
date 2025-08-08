5bc01520ef08fc7053085825149e42dce896922e
Ruchita-Presolv <ruchita@presolv360.com>
2025-08-08 16:15:32 +0530

    Update LoginStepDef.java

--- src/test/java/org/stepdefinitions/LoginStepDef.java
+++ src/test/java/org/stepdefinitions/LoginStepDef.java
@@ -13,7 +13,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class LoginStefDef {
+public class LoginStepDef {
  
 
 	private  LoginPage lp_obj= new LoginPage(DriverFactory.getDriver());
